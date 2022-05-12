baa450e5aa66f025b8ade177b023cc099d1fd879
xihajun <www.junfan.com@gmail.com>
2022-05-12 08:33:03 +0000

    test

--- test_app.py
+++ test_app.py
@@ -8,14 +8,19 @@ import pandas as pd
 import pickle
 import matplotlib.pyplot as plt
 import pathlib
+import re
 
+file_pre = st.sidebar.file_uploader('Upload pre-GOC file:', ('txt'))
+file_post = st.sidebar.file_uploader('Upload post-GOC file:', ('txt'))
 
-file_pre = st.sidebar.file_uploader('Upload pre-GOC file:', ('txt'), key="1"))
-file_post = st.sidebar.file_uploader('Upload post-GOC file:', ('txt'), key="2"))
+def read_string(string):
+    """load the txt data and return a list of sentences"""
+    return re.split('[.;:!,"]',line)[0:-1]
+
 
 if file_pre and file_post:
     pre_data = StringIO(file_pre.getvalue().decode("utf-8"))
     post_data = StringIO(file_post.getvalue().decode("utf-8"))
-    pre_data = pre_data.read()
-    post_data = pre_data.read()
-    st.write(post_data)
+    pre_txt = pre_data.read()
+    post_txt = pre_data.read()
+    print(read_string(pre_txt))
