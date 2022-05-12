25c63793f364608407c1ea1d39f410495f546394
xihajun <www.junfan.com@gmail.com>
2022-05-12 08:42:09 +0000

    add similarity

--- test_app.py
+++ test_app.py
@@ -11,7 +11,6 @@ import pathlib
 import re
 from io import StringIO
 
-
 file_pre = st.sidebar.file_uploader('Upload pre-GOC file:', ('txt'))
 file_post = st.sidebar.file_uploader('Upload post-GOC file:', ('txt'))
 
@@ -23,6 +22,20 @@ def read_string(string):
 if file_pre and file_post:
     pre_data = StringIO(file_pre.getvalue().decode("utf-8"))
     post_data = StringIO(file_post.getvalue().decode("utf-8"))
-    pre_txt = pre_data.read()
-    post_txt = pre_data.read()
-    print(read_string(pre_txt))
+    pre_txt, post_txt = pre_data.read(), post_data.read()
+    pre_list, post_list = read_string(pre_txt), read_string(post_txt)
+    
+
+    score = []
+    dic = {}
+    for i in pre_list:
+        rowscore = []
+        for j in post_list:
+            dic[(i,j)] = similar(i,j)
+            dic[(j,i)] = dic[(i,j)]
+            rowscore.append(dic[(i,j)])
+        score.append(rowscore)
+        
+    import matplotlib.pyplot as plt
+    fig = plt.imshow(score)
+    st.pyplot(fig)
