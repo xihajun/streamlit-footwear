fd18540308742861ac03a3987356ed8f811d6aed
xihajun <mw18386@my.bristol.ac.uk>
2022-05-12 09:18:28 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -10,39 +10,8 @@ import matplotlib.pyplot as plt
 import pathlib
 
 
-'# re'
-'A footwear mark hunter - footwear mark segmentation!'
-
-
-
-file_pre = st.sidebar.file_uploader('Upload pre-GOC file:', ('txt'))
-file_post = st.sidebar.file_uploader('Upload post-GOC file:', ('txt'))
-
-if file_pre and file_post:
-    pre_data = StringIO(file_pre.getvalue().decode("utf-8"))
-    post_data = StringIO(file_post.getvalue().decode("utf-8"))
-    pre_data = pre_data.read()
-    post_data = pre_data.read()
-    st.write(post_data)
-import numpy as np
-import os
-import requests
-import streamlit as st
-import pandas as pd
-import numpy as np
-import pandas as pd
-import pickle
-import matplotlib.pyplot as plt
-import pathlib
-
-
-'# re'
-'A footwear mark hunter - footwear mark segmentation!'
-
-
-
-file_pre = st.sidebar.file_uploader('Upload pre-GOC file:', ('txt'))
-file_post = st.sidebar.file_uploader('Upload post-GOC file:', ('txt'))
+file_pre = st.sidebar.file_uploader('Upload pre-GOC file:', ('txt'), key="1"))
+file_post = st.sidebar.file_uploader('Upload post-GOC file:', ('txt'), key="2"))
 
 if file_pre and file_post:
     pre_data = StringIO(file_pre.getvalue().decode("utf-8"))
